80138576e114861a64cd2501e806798913a11412
Tamara Hall <thall2000@gmail.com>
2019-04-10 22:04:09 -0400

    index.handlebars appears

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,6 @@
 // Requiring path to so we can use relative routes to our HTML files
 var path = require("path");
-
+var db = require("../models");
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
@@ -8,10 +8,16 @@ module.exports = function(app) {
 
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    // if (req.user) {
+      // db.Example.findAll({}).then(function(dbExamples) {
+        res.render("index", {
+          msg: "Welcome!"
+          // examples: dbExamples
+        });
+      // });
+      // res.redirect("/members");
+    // }
+    // res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
   app.get("/login", function(req, res) {
