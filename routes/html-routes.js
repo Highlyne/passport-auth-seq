46e2b06ad191d3c39ea9ae3b1c897bad17657249
Tamara Hall <thall2000@gmail.com>
2019-04-12 10:46:19 -0400

    data is pulling from db and displaying

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,5 @@
 // Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
+//var path = require("path");
 var db = require("../models");
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -53,7 +53,19 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    if (req.user) {
+      db.Example.findAll({raw: true}).then(function(dbExamples) {
+        console.log("Here is example data ", dbExamples);
+        res.render("example", {
+          msg: "Welcome!",
+          examples: dbExamples
+        });
+    // res.render("example", {
+    //   msg: "Blue!"
+    // })
+  });
+}  
+    //res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 
 };
