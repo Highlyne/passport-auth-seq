dff5882b576f58598a1da5a0b90a1bf5bc43c309
Tamara Hall <thall2000@gmail.com>
2019-04-12 22:21:04 -0400

    all & one of api-controllers is working

---
+++ controllers/api-controllers.js
@@ -0,0 +1,47 @@
+var db = require("../models");
+var model = db.Example;
+
+
+var exampleController = {
+    all: function(cb) {
+        console.log("You have reached the controller");
+        model.findAll({}).then(function(dbExamples) {
+            cb(dbExamples);
+             });
+    },
+    one: function(id, cb) {
+        model.findOne({
+            where: {
+                id: id
+            }
+        }).then(function(results){
+            cb(results);
+        })
+    },
+    new: function(thing1, thing2, cb){
+        model.create({
+            text: thing1,
+            description: thing2,
+        })
+        .then(function(results){
+            cb(results);
+        })
+    },
+    change: function(id, thing1, cb){
+        model.update(
+            thing1,
+            {where: {id: id}}
+        ).then(function(results){
+            cb(results);
+        })
+    },
+    delete: function(id, cb){
+        model.destroy(
+            {where: {id: id}}
+        ).then(function(results){
+            cb(results);
+        });
+    }
+}
+// Export routes for server.js to use.
+module.exports = exampleController;
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,8 @@
 // Requiring our models and passport as we've configured it
 var db = require("../models");
 var passport = require("../config/passport");
+  
+var orm = require("../controllers/api-controllers");
 
 module.exports = function(app) {
   
@@ -15,6 +17,30 @@ module.exports = function(app) {
       db.Example.findAll({}).then(function(dbExamples) {
        res.json(dbExamples)
         });
+    }
+  });
+
+   // Route for getting some data about our user to be used client side
+   app.get("/api/controller", function(req, res) {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+    res.redirect("/members")      
+  }
+    else {
+      orm.all(function(exData){
+        res.json(exData)
+      })
+    }
+  });
+
+  // Route for getting some data about our user to be used client side
+  app.get("/api/:id", function(req, res) {
+    var id =req.params.id;
+    if (!req.user) {res.redirect("/members")}
+    else {
+      orm.one(id,function(exData){
+        res.json(exData)
+      })
     }
   });
 };
